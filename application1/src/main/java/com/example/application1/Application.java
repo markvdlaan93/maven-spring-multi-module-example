4c5d7e33a8f6ec2d3a2417c917b6dc4bd93be0ac
Mark van der Laan <markkvdlaan93@gmail.com>
2019-05-01 19:56:31 +0200

    Add and print object from shared package

--- application1/src/main/java/com/example/application1/Application.java
+++ application1/src/main/java/com/example/application1/Application.java
@@ -2,11 +2,14 @@ package com.example.application1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.shared.write.Example;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        Example example = new Example();
+        system.println.out(example.testOutput());
         SpringApplication.run(Application.class, args);
     }
 }
